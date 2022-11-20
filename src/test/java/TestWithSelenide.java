92da5fa65da58fe9ed6cfae58cffa60407bcb6c1
zaidu-posmotryu <cherchimda@gmail.com>
2022-11-20 20:25:02 +0400

    init commit

---
+++ src/test/java/TestWithSelenide.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestWithSelenide {
+
+    @Test
+    public void testIssueText() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+        $(byText("С Новым Годом (2022)")).click();
+        $("#issues-tab").click();
+        $(withText("issue_to_test_allure_report")).should(Condition.exist);
+    }
+}
